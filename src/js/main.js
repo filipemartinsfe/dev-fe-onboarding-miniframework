067839efdb0db9d83ea41409f60064b04c569d48
Filipe Martins <filipe.martins@virtual-identity.com>
2018-08-14 09:15:35 +0100

    Toggle Plain js and css animations

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,12 @@
 // document.querySelector('.text').innerHTML = 'Javascript is working!'
 
 // animatedScrollTo(500);
+
+var toggledHeight = document.getElementById('howBig').offsetHeight;
+var toggle = document.getElementById('howBig');
+
+
+var mainC1 = document.getElementById('mainC1');
+
+mainC1.style.cursor = 'pointer';
+mainC1.onclick = () =>  toggle.classList.toggle('off');
