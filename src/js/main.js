7b446da4e82669488198dbaa9f55ea7d6e42fc7c
Filipe Martins <filipe.martins@virtual-identity.com>
2018-08-22 15:14:12 +0100

    Finished JS part with toggling class's

--- src/js/main.js
+++ src/js/main.js
@@ -1,50 +1,35 @@
 // Plain Javascript
-{
-    const toggleAll = document.querySelectorAll('.mainConversion__toggled');
+const btnClickedFunction = ( () => {
+    const parent = document.querySelectorAll('.mainConversion');
 
-    document.querySelectorAll('.mainConversion').forEach(element => {
+   parent.forEach(element => {
         const button = element.querySelector('button');
-        const toggle = element.querySelector('.mainConversion__toggled');
 
         button.addEventListener('click', () => {
-            toggleAll.forEach(info => {
-                if(toggle !== info){
-                    info.classList.add('off');
-                }
-            });  
-            toggle.classList.toggle('off');
+            const clickedParent = button.closest(".mainConversion");
+            if(clickedParent.classList.contains('mainConversion--toggled')) {
+                clickedParent.classList.remove('mainConversion--toggled')
+
+            } else {      
+            parent.forEach(clickedParent => {
+                clickedParent.classList.remove('mainConversion--toggled');
+            });
+            clickedParent.classList.add('mainConversion--toggled');
+
+            }
         });
     });  
-}
-
-// Plain Javascript attempt to change color
-// const toggleAll = document.querySelectorAll('.mainConversion__toggled');
-
-// document.querySelectorAll('.mainConversion').forEach(element => {
-//     const button = element.querySelector('button');
-//     const toggle = element.querySelector('.mainConversion__toggled');
-//     const toggleBefore = element.querySelector('.toggled__info');
- 
-//     button.addEventListener('click', () => {
-//         const color = window.getComputedStyle(button, null).getPropertyValue("background-color");
-//         toggleAll.forEach(info => {
-//             if(toggle !== info){
-//                 info.classList.add('off');
-//             }
-//         });  
-//         const pseudoElementContent = window.getComputedStyle(toggleBefore, ':before')
-//   .getPropertyValue('border-bottom');
-//   console.log(pseudoElementContent);
-//         toggle.style.backgroundColor = color;
-//         toggle.classList.toggle('off');
-//     });
-// });
-
-
+})();
 
 // Jquery
-// $('.mainConversion').click(function(){
-//     var others = $(this).siblings().find('.mainConversion__toggled');
-//     $(others).addClass('off');
-//     $(this).find('.mainConversion__toggled').toggleClass('off');
+// $('.mainConversion button').click(function(){
+//     $('.mainConversion').removeClass('mainConversion--toggled');
+//     const elParent = $(this).closest('.mainConversion');
+//     elParent.addClass('mainConversion--toggled');
 // });
+
+
+    let request = fetch('https://restcountries.eu/rest/v2/all').then( (res) => res.json());
+
+
+    // let request = fetch('https://restcountries.eu/rest/v2/all').then(res => res.json())
